d0658319cf0753ebf2f14ab1194215d7b3161ee8
ShayanRezvani <shayanrezvani5@gmail.com>
2024-08-09 20:02:11 +0330

    ⚡️ update

--- my-portfolio/tailwind.config.js
+++ my-portfolio/tailwind.config.js
@@ -1,9 +1,13 @@
-/** @type {import('tailwindcss').Config} */
+/**
+ * @format
+ * @type {import('tailwindcss').Config}
+ */
+
 export default {
-  content: [],
+  content: ["./src/**/"],
+
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
